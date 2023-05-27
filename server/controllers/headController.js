c871165ddb0187d03007ac915d012f0e71143747
omen <abubakr.sidiqque@gmail.com>
2023-05-27 23:09:56 +0500

    Compiled Controllers and Routes for Head under /server/controllers/headController and /server/routes/head

--- server/controllers/headController.js
+++ server/controllers/headController.js
@@ -1,4 +1,121 @@
-/* Controllers for Graph */
+const Head = require('../models/Head');
+const Student = require('../models/Head');
+const Class = require('../models/Class');
+
+/* Controllers for Head */
+
+/// View Dashboard
+const viewDashboard = async (req, res, next) => {
+
+    Head.find({}).populate("user").then(data => {
+        res.json(data);
+    }, err => next(err)).catch(err => {
+        res.send(err);
+    });
+}
+
+///  Get the Results of Students
+const getStudentResults = async (req, res, next) => {
+    try {
+        const studentId = req.params.id;
+
+        // Find the student by ID and populate the enrolledCourses field
+        const student = await Student.findById(studentId).populate({
+            path: "enrolledCourses",
+            populate: {
+                path: "assignments.aID quizzes.qID",
+                model: Course,
+            },
+        });
+
+        if (!student) {
+            res.status(404).json({ error: "Student not found" });
+        }
+
+        res.json(student.enrolledCourses);
+    } catch (error) {
+        console.error("Error fetching student results:", error);
+        res.status(500).json({ error: "Server error" });
+    }
+}
+
+/// Class By id
+/// get class by id
+const getClassById = (req, res, next) => {
+    Class.findById(req.params.cid).populate("students.sid").populate("teacher", "name").then(data => {
+        res.json(data);
+    }, err => next(err)).catch(err => {
+        console.log(err);
+    });
+
+}
+
+/// Course Results
+/// Function to fetch the result of a course by its ID
+const getCourseResultById = async (req, res, next) => {
+
+    try {
+        const courseId = req.params.id;
+        const course = await Course.findById(courseId)
+            .populate('quizzes.qID', 'grades.marks')
+            .populate('assignments.aID', 'grades.marks')
+            .populate('midTerm.grade.student', 'marks')
+            .populate('final.grade.student', 'marks');
+
+        if (!course) {
+            res.status(404).json({ error: "Course not found" });
+            //throw new Error('Course not found');
+        }
+
+        let totalQuizzesMarks = 0;
+        let totalAssignmentsMarks = 0;
+        let totalMidTermMarks = 0;
+        let totalFinalMarks = 0;
+        let totalStudents = 0;
+
+        // Calculate the sum of marks for quizzes
+        for (const quiz of course.quizzes) {
+            totalQuizzesMarks += quiz.qID.grades.reduce((total, grade) => total + grade.marks, 0);
+        }
+
+        // Calculate the sum of marks for assignments
+        for (const assignment of course.assignments) {
+            totalAssignmentsMarks += assignment.aID.grades.reduce((total, grade) => total + grade.marks, 0);
+        }
+
+        // Calculate the sum of marks for mid-term
+        for (const grade of course.midTerm.grade) {
+            totalMidTermMarks += grade.marks;
+            totalStudents++;
+        }
+
+        // Calculate the sum of marks for final
+        for (const grade of course.final.grade) {
+            totalFinalMarks += grade.marks;
+            totalStudents++;
+        }
+
+        const averageQuizzesMarks = totalQuizzesMarks / course.quizzes.length;
+        const averageAssignmentsMarks = totalAssignmentsMarks / course.assignments.length;
+        const averageMidTermMarks = totalMidTermMarks / totalStudents;
+        const averageFinalMarks = totalFinalMarks / totalStudents;
+
+        const courseResult = {
+            averageQuizzesMarks,
+            averageAssignmentsMarks,
+            averageMidTermMarks,
+            averageFinalMarks
+        };
+
+        return courseResult;
+        res.json(courseResult);
+    } catch (error) {
+        console.error('Error fetching course result:', error);
+        res.status(500).json({ error: "Server error" });
+        //throw error;
+    }
+}
+
 
 
 // For any month
@@ -32,5 +149,9 @@ async function getStudentCountForMonth(req, res) {
 
 
 module.exports = {
-    getStudentCountForMonth
+    getStudentCountForMonth,
+    getStudentResults,
+    getClassById,
+    getCourseResultById,
+    viewDashboard
 }
--- server/routes/head.js
+++ server/routes/head.js
@@ -2,11 +2,24 @@ var express = require("express");
 router = express.Router();
 const HeadController = require("../controllers/headController");
 
-router.get("/", (req, res, next) => {
-    res.send("Hello, Head.")
-})
 
-/* Graph Routes */
+/// Dashboard 
+/// Hanzala Javed
+router.get("/", HeadController.viewDashboard);
+
+/// Student Results
+/// Syed Kumail
+router.get('/results/student/:id', HeadController.getStudentResults);
+
+/// Class By ID
+router.get("/class/:cid", HeadController.getclassbyid);
+
+/// Course Results
+/// Uzair
+router.get("/results/course/:cid", HeadController.getCourseResultById);
+
+/// Graph
+/// [Abubakr Siddique FA20-BCS-098] 
 router.get("/graph", HeadController.getStudentCountForMonth);
 
 
