5526f90e6b205c45ebfa8464898fbb3ef1a646bf
Fareed Javed <fareedjaved203@gmail.com>
2023-05-28 21:39:43 +0500

    student route added to master

--- server/routes/student.js
+++ server/routes/student.js
@@ -1,8 +1,45 @@
 var express = require("express");
 router = express.Router();
+const User = require("../models/User");
+var CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+var studentController = require("../controllers/studentController");
 
 router.get("/", (req, res, next) => {
-    res.send("Hello, Student")
-})
+  res.send("Hello, Student");
+});
+
+// Authentication - Login (Hammad)
+router.get("/login", studentController.login);
+
+// Authentication - Signup (Ismail)
+router.post("/login", studentController.signIn);
+
+//Moeez Ahmed
+router.get("/userprofile", studentController.studentprofile);
+
+//Hassan Sajjad
+router.get(
+  "/student-dashboard/:courseid/coursematerial",
+  studentController.getmaterials
+);
+
+//Bilal Shakir
+router.get(
+  "/student-dashboard/:courseid/getassignment",
+  studentController.getassignment
+);
+
+// Show Dashboard "Adil's Route"
+router.get("/student-dashboard/:sid", studentController.showDashboard);
+
+// Show Grades "Adil's Route"
+router.get(
+  "/student-dashboard/:courseid/enrolled-course/grades",
+  studentController.viewGrades
+);
+
+//Bilal Gondal
+router.get("/student/:id/result", studentController.studentResult);
 
 module.exports = router;
